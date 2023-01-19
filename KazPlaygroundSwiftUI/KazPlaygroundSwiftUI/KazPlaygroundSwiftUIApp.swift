ea62d36c5f63c1e0120c717a4f4518f90ff09bc0
Taeyoung Won <hexter1994@gmail.com>
2023-01-19 23:51:26 +0900

    [Add] Project Init

---
+++ KazPlaygroundSwiftUI/KazPlaygroundSwiftUI/KazPlaygroundSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  KazPlaygroundSwiftUIApp.swift
+//  KazPlaygroundSwiftUI
+//
+//  Created by 원태영 on 2023/01/19.
+//
+
+import SwiftUI
+
+@main
+struct KazPlaygroundSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
