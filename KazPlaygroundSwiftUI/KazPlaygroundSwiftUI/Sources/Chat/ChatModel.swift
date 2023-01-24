218a7fbe8d71c72627569aa30c94b29742e51c75
Taeyoung Won <hexter1994@gmail.com>
2023-01-24 23:31:15 +0900

    [Chore] #4 Chat Model lastDate 프로퍼티 추가

--- KazPlaygroundSwiftUI/KazPlaygroundSwiftUI/Sources/Chat/ChatModel.swift
+++ KazPlaygroundSwiftUI/KazPlaygroundSwiftUI/Sources/Chat/ChatModel.swift
@@ -10,6 +10,7 @@ import Foundation
 struct Chat : Identifiable {
     let id : String // 채팅방 ID
     let date : Double // 생성 날짜
+    let lastDate : Double // 마지막 메세지 날짜
     let lastContent : String // 마지막 메세지 내용
     
     var stringDate : String {
@@ -19,6 +20,17 @@ struct Chat : Identifiable {
         dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
         
         let dateAt = Date(timeIntervalSince1970: date)
+        
+        return dateFormatter.string(from: dateAt)
+    }
+    
+    var stringLastDate : String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "ko_kr")
+        dateFormatter.timeZone = TimeZone(abbreviation: "KST")
+        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        
+        let dateAt = Date(timeIntervalSince1970: lastDate)
         
         return dateFormatter.string(from: dateAt)
     }
